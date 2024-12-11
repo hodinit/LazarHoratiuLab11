5015e92e4a7e285887ad385ebb861909ccb11d48
HoratiuLazar <horatiulazar2003@gmail.com>
2024-12-11 19:50:33 +0200

    Add project files.

---
+++ LazarHoratiuLab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LazarHoratiuLab11.Data;
+
+namespace LazarHoratiuLab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ LazarHoratiuLab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazarHoratiuLab11.Models;
+
+namespace LazarHoratiuLab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
